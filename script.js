d150407a4eae90bde7b95b38a2a54dd22dd7c450
lewinskie254 <142066223+lewinskie254@users.noreply.github.com>
2023-10-21 01:37:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const modal = document.querySelector('.modal')
+const input = document.getElementById('email')
+const dangerToggle = document.querySelector('.form input')
+const validEmail = document.querySelector('.dark')
+const subscribe= document.querySelector('.subscribe')
+const dismiss = document.querySelector('.dismiss')
+const incomplete = document.querySelector('.incomplete ')
+
+
+subscribe.addEventListener('click', ()=>{
+    const inputValue = input.value 
+    if(!inputValue.includes('@')){
+        validEmail.classList.remove('dark')
+        validEmail.classList.add('error')
+        dangerToggle.classList.add('danger')
+        
+    }else {
+        incomplete.classList.add('hide')
+        modal.classList.remove('modal')
+        modal.classList.add('successful')
+    }
+})
+
+input.addEventListener('keyup', (e)=>{
+    e.preventDefault()
+    validEmail.classList.add('dark')
+    validEmail.classList.remove('error')
+    dangerToggle.classList.remove('danger')
+})
+
+
+dismiss.addEventListener('click', ()=>{
+        validEmail.classList.add('dark')
+        validEmail.classList.remove('error')
+        dangerToggle.classList.remove('danger')
+        modal.classList.add('modal')
+        modal.classList.remove('successful')
+        incomplete.classList.add('incomplete')
+        incomplete.classList.remove('hide')
+        input.value  = ''
+})
